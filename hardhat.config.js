c77a7013dac3f72a8c71481553e054f096815966
GuillermoFairbairn <fairbairnguillermo@gmail.com>
2022-12-29 09:26:59 -0300

    {CONFIG} setup hardhat plugins and dependecies required

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,25 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.RPC_URL,
+      }
+    }
+  },
+  solidity: {
+    compilers: [
+      {version: "0.6.12"},
+      {version: "0.8.0"},
+      {version: "0.8.1"},
+      {version: "0.8.2"}
+    ],
+  },
+  mocha: {
+    timeout: 100000000
+  }
 };
