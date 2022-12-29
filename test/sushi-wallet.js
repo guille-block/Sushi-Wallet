467cd0bd26eb1bd433dd719156ba7cc0e3945c9a
GuillermoFairbairn <fairbairnguillermo@gmail.com>
2022-12-29 09:32:24 -0300

    {TEST} Sushi wrapper testing functionality

--- test/sushi-wallet.js
+++ test/sushi-wallet.js
@@ -103,4 +103,97 @@ describe('Sushi Wallet Tests', function () {
             expect(tokenBalance).to.be.greaterThan("0")
         }
     })
+    
+    //LP and YF on MasterchefV2 pool
+    it('Sushi wallet LP and YF in one transaction CVX/WETH (MASCTERCHEF V2)', async function () {
+        console.log("Next tests might require a couple of seconds, please wait...")
+        let weth = await this.erc20.attach(process.env.WETH) 
+        let CVX = await this.erc20.attach(process.env.CVX) 
+        this.wethBalance = await weth.balanceOf(this.deployerSushiWallet.address)
+        let CVXBalance = await CVX.balanceOf(this.deployerSushiWallet.address)
+        let pairAvailableAmount = this.wethBalance.div(2) > CVXBalance ? CVXBalance : this.wethBalance.div(2)
+        
+        //transaction that handles Liquidity providing and yield farming
+        await this.deployerSushiWallet.executeYieldFarming(
+            process.env.CVX,
+            process.env.WETH,
+            pairAvailableAmount,
+            pairAvailableAmount,
+            (await ethers.provider.getBlock('latest')).timestamp * 2
+        )
+    })
+
+    //LP and YF on MasterchefV1 pool
+    it('Sushi wallet LP and YF in one transaction USDC/WETH (MASCTERCHEF V1)', async function () {
+        let usdc = await this.erc20.attach(process.env.USDC) 
+        let usdcBalance = await usdc.balanceOf(this.deployerSushiWallet.address)
+         
+        //transaction that handles Liquidity providing and yield farming
+        await this.deployerSushiWallet.executeYieldFarming(
+            process.env.USDC,
+            process.env.WETH,
+            usdcBalance,
+            this.wethBalance.div(2),
+            (await ethers.provider.getBlock('latest')).timestamp * 2
+        )
+
+    })
+
+    
+    //Additional functionality check on SLP withdrawals to validate SUSHI balances at different moments. The
+    //end goal is to test end-to-end wallet functionality.
+    //Get logs to see amount of SLP tokens deposited and withdraw that same balance, then validate we received 
+    //the correct quantity of SUSHI tokens 
+    it('Sushi wallet withdraw from yield farms and retrieve SUSHI (both MASCTERCHEF V1 and V2)', async function () {
+        //Set starting block to get logs
+        let startingBlockToCheck = (await ethers.provider.getBlock()).number - 1000 // go back a couple blocks to catch events
+        let startingBlockToCheckHex = ethers.BigNumber.from(startingBlockToCheck)._hex
+        //ethers.js doesnt support checking on multiple addresses at the same time. For event catching, an 
+        //alternative is to use web3.js and handle everything with one method. For simplicity, 
+        //it is used getLogs() from ethers.js to catch deposits to MasterchefV1 and MaterchefV2.
+        let depositLogMasterChefV1 = await ethers.provider.getLogs(
+            {
+                fromBlock: startingBlockToCheckHex,
+                address: process.env.MASTER_CHEF_V1, 
+                topics: [
+                    ethers.utils.id("Deposit(address,uint256,uint256)"), 
+                    ethers.utils.hexZeroPad(this.deployerSushiWallet.address, 32 )
+                ]
+            }
+        )
+        let depositLogMasterChefV2 = await ethers.provider.getLogs(
+            {
+                fromBlock: startingBlockToCheckHex,
+                address: process.env.MASTER_CHEF_V2, 
+                topics: [
+                    ethers.utils.id("Deposit(address,uint256,uint256,address)"),
+                    ethers.utils.hexZeroPad(this.deployerSushiWallet.address, 32 )
+                ]
+            }
+        )
+
+        this.sushiERC20 = await this.erc20.attach(process.env.SUSHI)
+        //Intial SUSHI balance has to be 0
+        let SUSHIBalanceInitial = await this.sushiERC20.balanceOf(this.deployerSushiWallet.address)
+        expect(SUSHIBalanceInitial).to.be.equal("0")
+        //Dummy test to make sure we received a positive amount of SUSHI tokens after withdrawal from 
+        //MasterChefV1
+        const amountSLPForUSDCWETH = ethers.BigNumber.from(ethers.utils.hexStripZeros(depositLogMasterChefV1[0].data))
+        await this.deployerSushiWallet.withdrawFromYieldFarming(process.env.USDC, process.env.WETH, amountSLPForUSDCWETH)
+        let SUSHIBalanceAfterV1Withdrawal = await this.sushiERC20.balanceOf(this.deployerSushiWallet.address)
+        expect(SUSHIBalanceAfterV1Withdrawal).to.be.greaterThan("0")
+        //Dummy test to make sure we have a greater balance of SUSHI tokens after withdrawal from MasterChefV2
+        //Under the hood we are calling withdrawAndHarvest() to "harvest" SUSHI rewards
+        const amountSLPForCVXWETH = ethers.BigNumber.from(ethers.utils.hexStripZeros(depositLogMasterChefV2[0].data))
+        await this.deployerSushiWallet.withdrawFromYieldFarming(process.env.CVX, process.env.WETH, amountSLPForCVXWETH) 
+        this.SUSHIBalanceAfterV2Withdrawal = await this.sushiERC20.balanceOf(this.deployerSushiWallet.address)
+        expect(this.SUSHIBalanceAfterV2Withdrawal).to.be.greaterThan(SUSHIBalanceAfterV1Withdrawal)
+    })
+
+    it('Sushi wallet withdraw SUSHI funds correctly to owner', async function () {        
+        expect(await this.sushiERC20.balanceOf(deployer.address)).to.be.equal(0)
+        await this.deployerSushiWallet.withdrawERC20(process.env.SUSHI, this.SUSHIBalanceAfterV2Withdrawal)
+        expect(await this.sushiERC20.balanceOf(deployer.address)).to.be.equal(this.SUSHIBalanceAfterV2Withdrawal)
+    })
+    
 })
